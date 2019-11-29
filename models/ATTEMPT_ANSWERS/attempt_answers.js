2cd307f3b558b4405cc94d15b7cd7270ac4eb429
Christopher Salazar <csalaz14@asu.edu>
2019-11-29 13:31:53 -0800

    Create attempt_answers data table

---
+++ models/ATTEMPT_ANSWERS/attempt_answers.js
@@ -0,0 +1,24 @@
+/*
+    Copyright 2019 SER401 Project 14 Team - All Rights Reserved
+
+    Team Members: 
+    RAYMOND ACEVEDO
+    SHAWN WEINER
+    CHRISTOPHER SALAZAR
+    ROBERT PILLITTERI
+    SHELTON LACY 
+
+    Unauthorized copying of this file, via any medium is strictly prohibited
+    Proprietary and confidential
+*/
+
+
+class Attempt_Answers extends Model {}
+Attempt_Answers.init({
+	student_answer: Sequelize.INTEGER
+	correct: Sequelize.INTEGER
+}, {
+	sequelize, 
+	modelName: ‘attempt_answers’ 
+});
+Attempt_Answers.belongsTo(Question);
